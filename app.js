aacdceb8c67665929047f436160f555e8aab74e5
jpcamara <johnpcamara@gmail.com>
2014-04-14 13:01:26 -0400

    added in some logging

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var cluster = require('cluster'),
     os      = require('os'),
-    server  = require('./lib/server');
+    server  = require('./lib/server'),
+    logger  = require('./lib/logger');
 
 if (cluster.isWorker) {
   return server();
@@ -10,10 +11,12 @@ for (var i = 0; i < os.cpus().length; i++) {
   cluster.fork();
 }
 
-cluster.on('exit', function (/*worker, code, signal*/) {
+cluster.on('exit', function (worker, code, signal) {
+  logger.error('Worker %s died with code %s', worker.process.pid, code);
   cluster.fork();
 });
 
 process.on('uncaughtException', function (err) {
+  logger.fatal(err);
   process.exit(err);
 });
